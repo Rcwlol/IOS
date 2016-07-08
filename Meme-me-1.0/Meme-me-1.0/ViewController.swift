6b7d1253f547883106e4ec418aa0c8c2e0724fa6
Rcwlol <rcwreality@gmail.com>
2016-07-08 12:13:08 -0700

    update Meme-me

--- Meme-me-1.0/Meme-me-1.0/ViewController.swift
+++ Meme-me-1.0/Meme-me-1.0/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, UITextFieldDelegate {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, UITextFieldDelegate{
     struct Meme {
         var topText: String!
         var image: UIImage!
@@ -22,11 +22,14 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     @IBOutlet weak var imagePicked: UIImageView!
     @IBOutlet weak var topTextField: UITextField!
     
+    @IBOutlet weak var toolBar: UIToolbar!
+    @IBOutlet weak var shareButton: UIButton!
     @IBOutlet weak var botTextField: UITextField!
     var currentTextField = UITextField()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         topTextField.delegate = self
         botTextField.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
@@ -34,7 +37,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
             NSStrokeColorAttributeName : UIColor.blackColor(),
             NSForegroundColorAttributeName : UIColor.whiteColor(),
             NSFontAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
-            NSStrokeWidthAttributeName : 1.0
+            NSStrokeWidthAttributeName : 3.0
             
         ]
         topTextField.defaultTextAttributes = memeTextAttributes
@@ -44,6 +47,12 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         super.viewWillAppear(animated)
         self.subscribeToKeyboardNotifications()
         camButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
+        if imagePicked.image != nil{
+            shareButton.enabled = true
+        }else{
+            shareButton.enabled = false
+        }
+        
         
     }
     override func viewWillDisappear(animated: Bool) {
@@ -54,6 +63,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     }
 
     @IBAction func pickAnImageAlbum(sender: AnyObject) {
+       
         let pickerController = UIImagePickerController()
         pickerController.delegate = self
         pickerController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
@@ -67,6 +77,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         }
     }
     @IBAction func pickImageCam(sender: AnyObject) {
+        
         let pickController = UIImagePickerController()
         pickController.delegate = self
         pickController.sourceType = UIImagePickerControllerSourceType.Camera
@@ -119,14 +130,35 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     }
     func generateMemedImage() -> UIImage{
     // Render view to an image
-    UIGraphicsBeginImageContext(self.view.frame.size)
-    view.drawViewHierarchyInRect(self.view.frame,
-    afterScreenUpdates: true)
-    let memedImage : UIImage =
-    UIGraphicsGetImageFromCurrentImageContext()
-    UIGraphicsEndImageContext()
+        toolBar.hidden = true
+        shareButton.hidden = true
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawViewHierarchyInRect(self.view.frame, afterScreenUpdates: true)
+        let memedImage : UIImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        toolBar.hidden = false
+        shareButton.hidden = false
+    
     
     return memedImage
+    }
+    @IBAction func youGetMeme(sender: AnyObject) {
+        let mememe = generateMemedImage()
+        let share = UIActivityViewController(activityItems: [mememe], applicationActivities: nil)
+        self.presentViewController(share, animated : true, completion: nil)
+        
+        share.completionWithItemsHandler = {(activityType, completed:Bool, returnedItems:[AnyObject]?, error: NSError?) in
+            
+            if (completed) {
+                self.save()
+                self.dismissViewControllerAnimated(true, completion: nil)
+            }else{
+                return
+            }
+            
+            
+        }
     }
 
 
